3c7565c224e3417dd09ea6e41a34f0a1ec181966
Tijmen <30957@ma-web.nl>
2021-01-15 16:07:14 +0100

    Quiz finished, Homepage minor changes

---
+++ Sprint 3/QUIZ/js/vragen.js
@@ -0,0 +1,60 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+		"title": "Wat is uw favourite kunstwerk?",
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Houd u van porselein?",
+				"answers": [{
+						"answer": "Ja",
+						"feedback": true
+					},
+					{
+						"answer": "Nee",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Houd u van objecten?",
+				"answers": [{
+						"answer": "Ja",
+						"feedback": false
+					},
+					{
+						"answer": "Nee",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "Houd u van pakketjes?",
+				"answers": [{
+						"answer": "Ja",
+						"feedback": true
+					},
+					{
+						"answer": "Nee",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Houd van een groot of een klein kunstwerk?",
+				"answers": [{
+						"answer": "Klein",
+						"feedback": true
+					},
+					{
+						"answer": "Groot",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
